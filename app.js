d1bc2eb9540d63daf9c09b6ca92a763b2446463a
Tayyab Naseem Minhas <tayyab@fori.co>
2022-03-01 19:41:57 +0500

    Initial test Server

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const person = require('./person')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  console.log(person)
+  res.send('Hello World!')
+})
+
+app.get('/new', (req, res) => {
+  res.send('Hello to New World!')
+})
+
+app.get('/newnew', (req, res) => {
+  res.send('Hello to New World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
